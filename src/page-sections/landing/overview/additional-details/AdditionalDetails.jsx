7823d702d93c5d1daa68f4b3f48f271b012de5c0
RaiAbdullah <nawazabdullah1800@gmail.com>
2025-10-10 20:40:24 +0500

    corrected the dates from additional details of educatation section

--- src/page-sections/landing/overview/additional-details/AdditionalDetails.jsx
+++ src/page-sections/landing/overview/additional-details/AdditionalDetails.jsx
@@ -24,18 +24,18 @@ export default function AdditionalDetails() {
       <Stack mt={3} spacing={2}>
         {/* <ListItem title="Email" Icon={EmailOutlined} subTitle="Uilib@gmail.com" color={theme.palette.grey[400]} /> */}
 
-        {/* <ListItem Icon={Globe} title="Language" subTitle="English, Spanish" color={theme.palette.primary.main} /> */}
+        
 
         {/* <ListItem title="Nickname" subTitle="Pixy" Icon={UserOutlined} color={theme.palette.warning[600]} /> */}
 
         {/* <ListItem Icon={DateRange} title="Join Date" subTitle="Aug 15th, 2021" color={theme.palette.success.main} /> */}
 
-        <ListItem Icon={Education} title="Lab Assistent (PF) Nov 2023 - Mar 2024" email="adeel.ahmed@uog.edu.pk" subTitle="Under the supervision of Adeel Ahmed at University of Gujrat" color={theme.palette.warning.main} />
+        <ListItem Icon={Education} title="Lab Assistent (PF) Nov 2022 - Mar 2023" email="adeel.ahmed@uog.edu.pk" subTitle="Under the supervision of Adeel Ahmed at University of Gujrat" color={theme.palette.warning.main} />
           <hr />
         <ListItem Icon={Education}  title="Teaching Assistant, AI Lab Nov 2023 - Mar 2024" email="zahid.iqbal@uog.edu.pk" subTitle="Under the supervision of Dr. Zahid Iqbal at University of Gujrat" color={theme.palette.info.main}  />
           <hr />
-        <ListItem title="MERN Stack Developer, 2023 - 2024 " emailAlt="at BIC Business Incubation Center" subTitle="University of Gujrat" Icon={BriefcaseOutlined} color={theme.palette.error.main} 
-/>
+        <ListItem title="MERN Stack Developer, 2023 - 2024 " emailAlt="at BIC Business Incubation Center" subTitle="University of Gujrat" Icon={BriefcaseOutlined} color={theme.palette.error.main} />
+        
       </Stack>
     </Card>;
 }

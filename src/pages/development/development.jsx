a2357f0f28ba36984a0a0a2d7d62882aa6da0e42
RaiAbdullah <nawazabdullah1800@gmail.com>
2026-02-11 15:08:39 +0500

    refactor: Rename development page title to projects
    - Update browser tab title from 'Development' to 'Projects'
    - Better reflect page content focused on project portfolio

--- src/pages/development/development.jsx
+++ src/pages/development/development.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import DevelopmentPageView from '@/page-sections/development/page-view';
 export default function DevelopmentPage() {
   useEffect(() => {
-    document.title = 'Rai Abdullah Nawaz | Development';
+    document.title = 'Rai Abdullah Nawaz | Projects';
   }, []);
   return <DevelopmentPageView />;
 }
